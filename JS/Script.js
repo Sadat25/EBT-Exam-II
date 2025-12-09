00e080f071273c429c0ed11c654e7c7814364a1e
Sadat Shahriar Rafin <sadatshahriarrafin@gmail.com>
2025-12-09 10:39:34 +0600

    first commit

---
+++ JS/Script.js
@@ -0,0 +1,30 @@
+// code for time 
+
+const d = new Date();
+let text = d.toLocaleString();
+document.getElementById("demo").innerHTML = text;
+
+// code for popup
+
+function showAlert() {
+    alert("Message Sent Successfully!");
+}
+
+
+// counter
+
+let finalValue = 5000;
+let startValue = 0;
+let duration = 2500;
+let stepTime = Math.abs(Math.floor(duration / finalValue));
+
+function upValue() {
+    if (startValue <= finalValue) {
+        document.getElementById("number_up").innerText = startValue;
+        startValue++;
+        setTimeout(upValue, stepTime);
+    }
+}
+upValue();
+
+
